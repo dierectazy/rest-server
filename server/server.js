b0444a77c1da0fcb48a9e6e2ec240b71b02e52c8
Dier <jg214782@gmail.com>
2019-01-04 13:48:26 -0500

    NPM start

--- server/server.js
+++ server/server.js
@@ -44,6 +44,6 @@ app.delete('/usuario', function(req, res) {
     res.json("delete Usuario");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Escuchando en el puerto 3000");
 })
